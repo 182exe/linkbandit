8a78b2ffc18ebb652ee1802826dfec1b90000b55
182exe <81325987+182exe@users.noreply.github.com>
2022-03-29 13:17:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnop';
+const number = '0123456789';
+
+let settingLength = 3;
+let content = '';
+
+function generateString(length, link, extension) {
+  let result = '';
+  const uppercaseLength = uppercase.length;
+  const lowercaseLength = lowercase.length;
+  const numberLength = number.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += link;
+    for ( let i = 0; i < extension; i++ ) {
+      let randInt = Math.floor(Math.random() * 3);
+      if (randInt === 0) {
+        result += uppercase.charAt(Math.floor(Math.random() * uppercaseLength));
+      } else {
+        if (randInt === 1) {
+          result += lowercase.charAt(Math.floor(Math.random() * lowercaseLength));
+        } else {
+          if (randInt === 2) {
+            result += number.charAt(Math.floor(Math.random() * numberLength));
+          }
+        }
+      }
+    }
+    result += ' \n';
+  }
+  return result;
+}
+
+function start(lengthsetting, linksetting, extensionsetting) {
+  const raw = (generateString(lengthsetting, linksetting, extensionsetting));
+  const str = raw;
+  const text = document.querySelector(".text");
+  const input = document.querySelector(".input");
+  content = str;
+  function populateText(str) {
+      let span = document.createElement("span");
+      span.setAttribute("id", "result");
+      span.innerText = str;
+      text.appendChild(span);
+  }
+  populateText(str);
+}
+
+function updateSettings() {
+  if (document.getElementById("lengthInput").value.length === 0) {
+    window.alert('You didn\'t provide any valid length input in the box, so I\'ve defaulted to generating you 3 links.');
+    start(3);
+  } else {
+    let settingLength = document.getElementById("lengthInput").value;
+    let settingLink = document.getElementById("urlInput").value;
+    let settingExtension = document.getElementById("extensionInput").value;
+    start(settingLength, settingLink, settingExtension);
+  }
+}
+
+function viewRaw() {
+  content = document.getElementById("result").innerText;
+  let rawWindow = window.open("","");
+  rawWindow.document.write(content);
+}
+
+function copyResult() {
+  const copyText = document.getElementById("result").innerText;
+  navigator.clipboard.writeText(copyText);
+}
+
+function loadPremade() {
+  selectElement = document.querySelector('#premadeModes');
+  output = selectElement.value;
+  if (output == "hypixelDaily") {
+    document.getElementById("lengthInput").value = hypixelDaily[0];
+    document.getElementById("urlInput").value = hypixelDaily[1];
+    document.getElementById("extensionInput").value = hypixelDaily[2];
+  }
+  if (output == "discordNitro") {
+    document.getElementById("lengthInput").value = discordNitro[0];
+    document.getElementById("urlInput").value = discordNitro[1];
+    document.getElementById("extensionInput").value = discordNitro[2];
+  }
+}
+
+//-----PREMADE SETTINGS DEFAULTS-----//
+
+const hypixelDaily = ["10", "https://rewards.hypixel.net/claim-reward/", "8"];
+const discordNitro = ["10", "https://discord.gift/", "16"];
+
+//-----PREMADE SETTINGS DEFAULTS-----//
